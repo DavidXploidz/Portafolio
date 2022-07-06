b56bba3fb5a38b0dd234fc084439a3750b35a490
DavidXploidz <david.adame.itd@gmail.com>
2022-07-06 13:55:04 -0500

    actualice los proyectos

--- src/components/dataProyectos.js
+++ src/components/dataProyectos.js
@@ -8,31 +8,45 @@ const PROYECTOS = [
     },
     {
         id: 2,
+        titulo: 'Space Tourism multiple pages',
+        descripcion: 'Sitio responsive con multiples paginas con contenido cargado desde un archivo json  ',
+        imagen: 'https://res.cloudinary.com/dypooembo/image/upload/v1657133273/space_3_jlnxmh.png',
+        enlace: 'https://davidxploidz.github.io/space-webpage/'
+    },
+    {
+        id: 3,
         titulo: 'Social media dashboard with theme switcher',
         descripcion: 'Social media dashboard donde se muestra informacion estadistica de redes sociales, incluye un boton de dark mode',
         imagen: 'https://res.cloudinary.com/dypooembo/image/upload/v1656094483/social-media_ybzkbu.png',
         enlace: 'https://davidxploidz.github.io/Social-media-dashboard-with-theme-switcher/'
     },
     {
-        id: 3,
+        id: 4,
         titulo: 'Blog de Cafe',
         descripcion: 'Un sitio web responsive con información sobre preparación de café',
         imagen: 'https://res.cloudinary.com/dypooembo/image/upload/v1656094840/cafe_setsml.png',
         enlace: 'https://blogcaferesponsivez.netlify.app/'
     },
     {
-        id: 4,
+        id: 5,
         titulo: 'Crowdfunding product page',
         descripcion: 'Pagina web responsive con ventanas modales de precios y agradecimientos',
         imagen: 'https://res.cloudinary.com/dypooembo/image/upload/v1656094988/crowfunding_cytz9a.png',
         enlace: 'https://davidxploidz.github.io/Crowdfunding-product-page'
     },
     {
-        id: 5,
+        id: 6,
         titulo: 'Tip calcultator app',
         descripcion: 'Aplicacion web para calcular la propina y que muestra el total a pagar por persona',
         imagen: 'https://res.cloudinary.com/dypooembo/image/upload/v1656095475/calcultator_xdge47.png',
         enlace: 'https://davidxploidz.github.io/Tip-calculator-app/'
+    },
+    {
+        id: 7,
+        titulo: 'Bookmark landing page',
+        descripcion: 'Landing page responsive, valida la entrada de correo , tiene una seccion de preguntas frecuentes interactiva ',
+        imagen: 'https://res.cloudinary.com/dypooembo/image/upload/v1656353413/bookmark-landing_hnpg6u.png',
+        enlace: 'https://davidxploidz.github.io/Bookmark-landing-page/'
     }
 ]
 
